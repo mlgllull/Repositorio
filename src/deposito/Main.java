8e03e00158ee55a22429aad05355a96b5cc254c2
mlgllull <luisagomez_82@hotmail.com>
2022-02-17 01:22:50 +0100

    segundo commit

--- src/deposito/Main.java
+++ src/deposito/Main.java
@@ -12,8 +12,7 @@ public class Main {
 
     private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
-        double saldoActual;
-        
+        double saldoActual;       
         
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
